2b7539efdd4a5aaf5bdaf8ce10036ef065773e2e
mariajuliacarvalhoo14 <~161620231069@faculdadedamas.edu.br>
2024-04-29 17:55:08 -0300

    versao03

---
+++ ContaBanco/src/contaTerminal.java
@@ -0,0 +1,10 @@
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception{
+        int numeroConta;
+        char agencia;
+        char nome;
+        float saldo;
+        System.out.println("Por favor, digite o número da Agência !");
+    }
+
+}
